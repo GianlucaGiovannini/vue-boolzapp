92ef25759f3d1196085ea57785b5a9be47076a60
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-04-27 15:01:31 +0200

    code review

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,183 +14,196 @@ const app = new Vue({ //  (option object)
 
         contacts: [{
                 name: 'Michele',
-                id: "",
                 avatar: '_1',
                 visible: true,
                 messages: [{
                         date: '10/01/2020 15:30:55',
                         hours: "15:30",
                         message: 'Hai portato a spasso il cane?',
-                        status: 'sent'
+                        status: 'sent',
+                        optionMessage: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         hours: "15:50",
                         message: 'Ricordati di stendere i panni',
-                        status: 'sent'
+                        status: 'sent',
+                        optionMessage: false,
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         hours: "16:15",
                         message: 'Tutto fatto!',
-                        status: 'received'
+                        status: 'received',
+                        optionMessage: false,
                     }
                 ],
             },
             {
                 name: 'Fabio',
-                id: "",
                 avatar: '_2',
                 visible: true,
                 messages: [{
                         date: '20/03/2020 16:30:00',
                         hours: "16:30",
                         message: 'Ciao come stai?',
-                        status: 'sent'
+                        status: 'sent',
+                        optionMessage: false,
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         hours: "16:30",
                         message: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received'
+                        status: 'received',
+                        optionMessage: false,
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         hours: "16:35",
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'sent'
+                        status: 'sent',
+                        optionMessage: false,
                     }
                 ],
             },
             {
                 name: 'Samuele',
-                id: "",
                 avatar: '_3',
                 visible: true,
                 messages: [{
                         date: '28/03/2020 10:10:40',
                         hours: "10:10",
                         message: 'La Marianna va in campagna',
-                        status: 'received'
+                        status: 'received',
+                        optionMessage: false,
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         hours: "10:20",
                         message: 'Sicuro di non aver sbagliato chat?',
-                        status: 'sent'
+                        status: 'sent',
+                        optionMessage: false,
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         hours: "16:15",
                         message: 'Ah scusa!',
-                        status: 'received'
+                        status: 'received',
+                        optionMessage: false,
                     }
                 ],
             },
             {
                 name: 'Alessandro B.',
-                id: "",
                 avatar: '_4',
                 visible: true,
                 messages: [{
                         date: '10/01/2020 15:30:55',
                         hours: "15:30",
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
+                        status: 'sent',
+                        optionMessage: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         hours: "15:50",
                         message: 'Si, ma preferirei andare al cinema',
-                        status: 'received'
+                        status: 'received',
+                        optionMessage: false,
                     }
                 ],
             },
             {
                 name: 'Alessandro L.',
-                id: "",
                 avatar: '_5',
                 visible: true,
                 messages: [{
                         date: '10/01/2020 15:30:55',
                         hours: "15:30",
                         message: 'Ricordati di chiamare la nonna',
-                        status: 'sent'
+                        status: 'sent',
+                        optionMessage: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         hours: "15:50",
                         message: 'Va bene, stasera la sento',
-                        status: 'received'
+                        status: 'received',
+                        optionMessage: false,
                     }
                 ],
             },
             {
                 name: 'Claudia',
-                id: "",
                 avatar: '_6',
                 visible: true,
                 messages: [{
                         date: '10/01/2020 15:30:55',
                         hours: "15:30",
                         message: 'Ciao Claudia, hai novità?',
-                        status: 'sent'
+                        status: 'sent',
+                        optionMessage: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         hours: "15:50",
                         message: 'Non ancora',
-                        status: 'received'
+                        status: 'received',
+                        optionMessage: false,
                     },
                     {
                         date: '10/01/2020 15:51:00',
                         hours: "15:51",
                         message: 'Nessuna nuova, buona nuova',
-                        status: 'sent'
+                        status: 'sent',
+                        optionMessage: false,
                     }
                 ],
             },
             {
                 name: 'Federico',
-                id: "",
                 avatar: '_7',
                 visible: true,
                 messages: [{
                         date: '10/01/2020 15:30:55',
                         hours: "15:30",
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                        status: 'sent'
+                        status: 'sent',
+                        optionMessage: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         hours: "15:50",
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                        status: 'received'
+                        status: 'received',
+                        optionMessage: false,
                     }
                 ],
             },
             {
                 name: 'Davide',
-                id: "",
                 avatar: '_8',
                 visible: true,
                 messages: [{
                         date: '10/01/2020 15:30:55',
                         hours: "15:30",
                         message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                        status: 'received'
+                        status: 'received',
+                        optionMessage: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         hours: "15:50",
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                        status: 'sent'
+                        status: 'sent',
+                        optionMessage: false,
                     },
                     {
                         date: '10/01/2020 15:51:00',
                         hours: "15:51",
                         message: 'OK!!',
-                        status: 'received'
+                        status: 'received',
+                        optionMessage: false,
                     }
                 ],
             }
@@ -207,10 +220,11 @@ const app = new Vue({ //  (option object)
             const hoursTransform = `${data.getHours()}:${data.getMinutes()}`
 
             const newMessage = {
-                date: this.dataTransform,
-                hours: this.hoursTransform,
+                date: dataTransform,
+                hours: hoursTransform,
                 message: this.textInput,
-                status: 'sent'
+                status: 'sent',
+                optionMessage: false,
             }
             this.contacts[this.active].messages.push(newMessage)
 
@@ -223,13 +237,25 @@ const app = new Vue({ //  (option object)
                     date: dataTransform,
                     hours: hoursTransform,
                     message: "ok 😉",
-                    status: 'recieved'
+                    status: 'recieved',
+                    optionMessage: false,
                 }
 
                 setTimeout(() => { this.contacts[this.active].messages.push(replyMessage) }, 1000)
-
             }
 
+        },
+
+        messageOption(i) {
+            if (this.contacts[this.active].messages[i].optionMessage == false) {
+                this.contacts[this.active].messages[i].optionMessage = true
+            } else {
+                this.contacts[this.active].messages[i].optionMessage = false
+            }
+        },
+
+        deleteMessage(i) {
+            this.contacts[this.active].messages.splice(i, 1)
         },
 
 
